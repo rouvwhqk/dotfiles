c55b486a55421c85cb3bee8ce6558e4e896b255a
rouvwhqk <ro.uv@outlook.com>
2025-08-27 16:57:15 +0800

    I'm going back to Windows.

---
+++ dot_config/sway/README.md
@@ -0,0 +1,5 @@
+from
+
+packages
+wob
+
---
+++ root_etc/greetd/executable_start-sway.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# put this file to "/etc/greetd/"
+
+export LANG=zh_CN.UTF-8
+
+#export XWAYLAND_SCALE=1
+#export XCURSOR_SIZE=32
+#export GDK_SCALE=1
+#export GDK_DPI_SCALE=1.25
+#export QT_SCALE_FACTOR=1.25
+#export QT_AUTO_SCREEN_SCALE_FACTOR=0
+#export QT_FONT_DPI=120
+
+exec sway
